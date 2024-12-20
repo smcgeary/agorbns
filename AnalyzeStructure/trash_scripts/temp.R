07f11889cb4392e84405043f2509ed69ccfb1d4d
McGeary <sem689@login02.o2.rc.hms.harvard.edu>
2024-12-19 22:06:06 -0500

    Remove all files from Git tracking

--- AnalyzeStructure/trash_scripts/temp.R
+++
@@ -1,3 +0,0 @@
-		mean_flanks_predict <- sapply(unique(flanks_I), function(flank) {
-		mean(means_I[temp_inds][which(flanks_I[temp_inds]==flank)])/length(temp_inds)
-		})
--- Repression/test_target_scan_2.sh
+++
@@ -1,4 +0,0 @@
-python Repression/kathy_scripts/target_scan/calculate_predictions.py \
---feature_file /lab/solexa_bartel/klin/miRNA_models_data_old/ts7_outputs/refseq_utr90/features.txt \
---coeff_file Repression/kathy_scripts/target_scan/multisite_params.txt \
---out_dir Repression/temp_targetscan_output_2/
